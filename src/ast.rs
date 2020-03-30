0116691f33740f6da271594a8821055110f3375e
xecua <courser4848@gmail.com>
2020-03-31 01:33:29 +0900

    Abstract Grammar from Official Docs

--- src/ast.rs
+++ src/ast.rs
@@ -1,140 +1,245 @@
 //! # syntree
 //! 抽象構文木の構成要素
+//! https://docs.python.org/ja/3/library/ast.html#abstract-grammar からパクってきた(自作は無理)
 
-pub enum AST {
-    Init, // for initialization
-    File(
-        Vec<Box<AST>>, // stmt
-    ),
+// represents file(module).
+pub type AST = Vec<ASTStmt>;
+
+// ASDL(Abstract-type and Scheme-Definition Language) builtin types
+// ID
+pub type ASTIdentifier = String;
+// メタ情報?
+pub type ASTInt = i32;
+pub type ASTString = String;
+// ハードコードされた値?
+pub enum ASTConstant {
+    Int(i32),
+    Float(f32),
+    String(String),
+}
+// ???
+// pub type ASTObject;
+
+// AST parts
+pub enum ASTStmt {
     FuncDef(
-        // funcdef from compound_stmt
-        String,   // name
-        Box<AST>, // typedargslist from parameters
-        Box<AST>, // func_body_suite(suite)
+        String,          // name
+        ASTArguments,    // arguments
+        Vec<ASTStmt>,    // body
+        Option<ASTExpr>, // returns
     ),
-    Parameters(
-        Option<Box<AST>>, // typedargslist
+    Return(
+        Option<ASTExpr>, // value
     ),
-    TypedArgsList(
-        Vec<String>, // NAME
+    Delete(
+        Vec<ASTExpr>, // targets
     ),
-    Stmt(
-        Box<AST>, // simple_stmt or if_stmt or while_stmt or for_stmt or funcdef
+    Assign(
+        Vec<ASTExpr>, // targets
+        ASTExpr,      // value
     ),
-    SimpleStmt(
-        Box<AST>, // small_stmt
+    AnnAssign(
+        ASTExpr,         // target
+        Option<ASTExpr>, // value
+        ASTInt,          // `'simple' indicates that we annotate simple name without parens`
     ),
-    SmallStmt(
-        // expr_stmt or del_stmt or pass_stmt or global_stmt or (break_stmt or continue_stmt, return_stmt (from flow_stmt))
-        Box<AST>,
+    For(
+        ASTExpr,      // target
+        ASTExpr,      // iter
+        Vec<ASTStmt>, // body
     ),
-    ExprStmt(
-        Box<AST>, // testlist_star_expr
-        Vec<Box<AST>>, // annassign or testlist_star_expr
+    While(
+        ASTExpr,      // test
+        Vec<ASTStmt>, // body
     ),
-    Annassign(
-        Box<AST>,         // test
-        Option<Box<AST>>, // testlist_star_expr
+    If(
+        ASTExpr,      // test
+        Vec<ASTStmt>, // body
+        Vec<ASTStmt>, // else
     ),
-    TestlistStarExpr(
-        Vec<Box<AST>>, // (test or star_expr)[s]
-    ),
-    DelStmt(
-        Box<AST>, // exprlist
-    ),
-    PassStmt,
-    BreakStmt,                    // from flow_stmt
-    ContinueStmt,                 // from flow_stmt
-    ReturnStmt(Option<Box<AST>>), // testlist_star_expr // from flow_stmt
-    GlobalStmt(Vec<String>),      // NAME
-    IfStmt(
-        // if_stmt from compound_stmt
-        (Box<AST>, Box<AST>),         // if condition (test), suite
-        Vec<(Box<AST>, Box<AST>)>,    // elif condition (test), suite
-        Option<(Box<AST>, Box<AST>)>, // else condition (test), suite
-    ),
-    WhileStmt(
-        // while_stmt from compound_stmt
-        Box<AST>, // test
-        Box<AST>, // suite
-    ),
-    ForStmt(
-        // for_stmt from compound_stmt
-        Box<AST>, // exprlist
-        Box<AST>, // testlist
-        Box<AST>, // suite
-    ),
-    Suite(
-        Vec<Box<AST>>, // simple_stmt or stmt[s]
-    ),
-    Test(
-        Box<AST>,         // or_test
-        Option<Box<AST>>, // condition (or_test)
-        Option<Box<AST>>, // else (test)
-    ),
-    OrTest(
-        Vec<Box<AST>>, // and_test[s]
-    ),
-    AndTest(
-        Vec<Box<AST>>, // not_test[s]
-    ),
-    NotTest(
-        Box<AST>, // not_test or comparison
-    ),
-    Comparison(
-        Box<AST>,                  // expr
-        Vec<(Box<AST>, Box<AST>)>, // comp_op, expr
-    ),
-    Lt,    // <
-    Gt,    // >
-    Eq,    // ==
-    Leq,   // <=
-    Geq,   // >=
-    Neq,   // !=
-    In,    // in
-    NotIn, // not in
-    Is,    // is
-    IsNot, // is not
-    StarExpr(
-        Box<AST>, // expr
+    Global(
+        Vec<ASTIdentifier>, // names
     ),
     Expr(
-        Vec<Box<AST>>, // xor_expr
+        ASTExpr, // value
     ),
-    XorExpr(
-        Vec<Box<AST>>, // and_expr
-    ),
-    AndExpr(
-        Vec<Box<AST>>, // shift_expr
-    ),
-    ShiftExpr(
-        Box<AST>,        // arith_expr
-        Vec<(Box<AST>, Box<AST>)>, // << or >>, arith_expr
-    ),
-    Lshift, // <<
-    Rshift, // >>
-    ArithExpr(
-        Box<AST>,        // term
-        Vec<(AST, AST)>, // + or -, term
-    ),
-    Plus,  // +
-    Minus, // -
-    Term(
-        Box<AST>,        // factor
-        Vec<(AST, AST)>, // * or / or %, factor
-    ),
-    Mul, // *
-    Div, // /
-    Mod, // %
-    Exprlist(
-        Vec<Box<AST>>, // expr or star_expr
-    ),
-    Testlist(
-        Vec<Box<AST>>, // test
-    )
+    Pass,
+    Break,
+    Continue,
 }
 
-enum NotTestContent {
-    Not(Box<AST>),
-    Comparison(Box<AST>),
+pub enum ASTExpr {
+    BoolOp(
+        ASTBoolOp,    // op
+        Vec<ASTExpr>, // values
+    ),
+    BinOp(
+        Box<ASTExpr>, // left
+        ASTOperator,  // op
+        Box<ASTExpr>, // right
+    ),
+    UnaryOp(
+        ASTUnaryOp,   // op
+        Box<ASTExpr>, // operand
+    ),
+    Dict(
+        Vec<ASTExpr>, // keys
+        Vec<ASTExpr>, // values
+    ),
+    Set(
+        Vec<ASTExpr>, // elts
+    ),
+    // そもそも内包表記あるんか????
+    ListComp(
+        Box<ASTExpr>,          // elt
+        Vec<ASTComprehension>, // generators
+    ),
+    SetComp(
+        Box<ASTExpr>,          // elt
+        Vec<ASTComprehension>, // generators
+    ),
+    DictComp(
+        Box<ASTExpr>,          // key
+        Box<ASTExpr>,          // value
+        Vec<ASTComprehension>, // generators
+    ),
+    Compare(
+        Box<ASTExpr>,  // left
+        Vec<ASTCmpOp>, // ops
+        Vec<ASTExpr>,  // comparators
+    ),
+    Call(
+        Box<ASTExpr>, // func
+        Vec<ASTExpr>, // args
+                      // omit keywords
+    ),
+    // ???
+    FormattedValue(
+        Box<ASTExpr>,         // value,
+        Option<ASTInt>,       // conversion,
+        Option<Box<ASTExpr>>, // format_spec
+    ),
+    JoinedStr(
+        Vec<ASTExpr>, // values
+    ),
+    Constant(
+        ASTConstant,       // value
+        Option<ASTString>, // kind
+    ),
+    //  -- the following expression can appear in assignment context
+    // ???
+    Attribute(
+        Box<ASTExpr>,   // value
+        ASTIdentifier,  // attr,
+        ASTExprContext, // ctx <- ???
+    ),
+    Subscript(
+        Box<ASTExpr>,   // value
+        ASTSlice,       // slice
+        ASTExprContext, // ctx <- ???
+    ),
+    Starred(
+        Box<ASTExpr>,   // value
+        ASTExprContext, // ctx <- ???
+    ),
+    Name(
+        ASTIdentifier,  // id
+        ASTExprContext, // ctx <- ???
+    ),
+    List(
+        Vec<ASTExpr>,   // elts
+        ASTExprContext, // ctx <- ???
+    ),
+    Tuple(
+        Vec<ASTExpr>,   // elts
+        ASTExprContext, // ctx <- ???
+    ),
 }
+
+// https://stackoverflow.com/questions/6679171/python-ast-several-semantics-unclear-e-g-expr-context
+// 変数の位置
+pub enum ASTExprContext {
+    Load,     // 左辺値
+    Store,    // 右辺値
+    Del,      // del文の対象
+    AugLoad,  // 実引数?
+    AugStore, // 仮引数?
+}
+
+pub enum ASTSlice {
+    Slice(
+        Option<Box<ASTExpr>>, // lower
+        Option<Box<ASTExpr>>, // upper
+        Option<Box<ASTExpr>>, // step
+    ),
+    ExtSlice(
+        Vec<ASTSlice>, // dims
+    ),
+    Index(
+        Box<ASTExpr>, // value
+    ),
+}
+
+pub enum ASTBoolOp {
+    And,
+    Or,
+}
+
+pub enum ASTOperator {
+    Add,
+    Sub,
+    Mult,
+    Div,
+    Mod,
+    LShift,
+    RShift,
+    BitOr,
+    BitXor,
+    BitAnd,
+}
+
+// invert = ~, not = `not`
+pub enum ASTUnaryOp {
+    Invert,
+    Not,
+    UAdd,
+    USub,
+}
+
+pub enum ASTCmpOp {
+    Eq,
+    NotEq,
+    Lt,
+    LtE,
+    Gt,
+    GtE,
+    Is,
+    IsNot,
+    In,
+    NotIn,
+}
+
+pub type ASTComprehension = (
+    ASTExpr,      // target
+    ASTExpr,      // iter
+    Vec<ASTExpr>, // ifs
+);
+
+pub type ASTArguments = (
+    Vec<ASTArg>, // args
+);
+// arguments = (
+//   arg* posonlyargs, : positional only arguments(before /)
+//   arg* args, : normal arguments
+//   arg? vararg, : variable length argument
+//   arg* kwonlyargs, : keyword only arguments(after *)
+//   expr* kw_defaults, : keyword only arguments default value
+//   arg? kwarg, : kwargs
+//   expr* defaults : default values
+// )
+// def hoge(positional_only_argument1, positional_only_argument2, /, argument1, argument2, *[vararg], keyword_only_argument1, keyword_only_argument2, **kwarg)
+// 今回は普通にargsのみ利用可能とする
+
+pub type ASTArg = (
+    ASTIdentifier, // arg
+);
